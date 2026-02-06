c4deb576f6d4603023273e6e9fb3414ec3565872
Jasmine Mixson <jmixson1179@gmail.com>
2026-02-06 00:43:57 -0800

    Add and send results to AltText component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
+'use client';
+import { useState } from 'react';
 import ImageUploadForm from '@/components/ImageUploadForm';
+import AltText from '@/components/AltText';
 
 export default function Page() {
-  return <ImageUploadForm />;
+  const [resultText, setResultText] = useState('');
+
+  return (
+    <>
+      <ImageUploadForm onResult={setResultText} />
+      <AltText result={resultText} />
+    </>
+  );
 }
