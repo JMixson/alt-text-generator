b9051fcbedde8a9459b2dd966deddab3c56c8ebe
Jasmine Mixson <jmixson1179@gmail.com>
2026-02-09 01:20:39 -0800

    Move default text into main return

--- components/AltText.tsx
+++ components/AltText.tsx
@@ -1,18 +1,18 @@
 import CopyButton from './ui/copy-button';
 
 function AltText({ result }: { result: string }) {
-  if (!result) {
-    return (
-      <p className="text-pretty">
-        <em>No image has been selected yet.</em>
-      </p>
-    );
-  }
-
   return (
     <div className="bg-muted/50 flex gap-3.5 rounded-lg border-transparent px-4 py-3.5">
-      <p className="text-pretty">{result}</p>
-      <CopyButton text={result} />
+      {result ? (
+        <>
+          <p className="text-pretty">{result}</p>
+          <CopyButton text={result} />
+        </>
+      ) : (
+        <p className="text-pretty">
+          <em>No image has been selected yet.</em>
+        </p>
+      )}
     </div>
   );
 }
